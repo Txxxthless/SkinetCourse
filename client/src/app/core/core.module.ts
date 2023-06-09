842fd3b2c4e5d52b6eb496beba9ff95837d5a81c
Txxxthless <lepash85@gmail.com>
2023-06-09 10:58:23 +0300

    Section twelve

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,6 +5,9 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -12,8 +15,9 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
-  imports: [CommonModule, RouterModule],
-  exports: [NavBarComponent],
+  imports: [CommonModule, RouterModule, BreadcrumbModule, NgxSpinnerModule],
+  exports: [NavBarComponent, SectionHeaderComponent, NgxSpinnerModule],
 })
 export class CoreModule {}
