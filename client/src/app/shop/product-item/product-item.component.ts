50693531cad4692de49e818771814fc43f48a8b0
Txxxthless <lepash85@gmail.com>
2023-06-12 12:28:40 +0300

    Section fourteen

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,11 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
 import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent {
   @Input() product?: Product;
+
+  constructor(private baskteService: BasketService) {}
+
+  addItemToBasket() {
+    this.product && this.baskteService.addItemToBasket(this.product);
+  }
 }
